4c67bc4e7868df3b36cb558a00f25667d0a1335d
John Tandau <tandau@liquidoperations.com>
2016-05-10 20:04:02 -0400

    Changes and additions to the SortSupport class, and added the SortSupportTest class for testing

--- src/main/java/org/ticdev/toolboxj/collections/SortSupport.java
+++ src/main/java/org/ticdev/toolboxj/collections/SortSupport.java
@@ -1,6 +1,9 @@
 package org.ticdev.toolboxj.collections;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.RandomAccess;
 
 /**
  * Support methods for sorting algorithms.
@@ -8,6 +11,50 @@ import java.util.Comparator;
  * @author <a href="mailto:tandauioan@gmail.com">Ioan - Ciprian Tandau</a>
  */
 public class SortSupport {
+
+    /**
+     * An integer sized collection cannot be larger than this value for binary
+     * search insertion point to work.
+     */
+    public static final int INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT
+            = Integer.MAX_VALUE + Integer.MIN_VALUE > 0
+                    ? -Integer.MIN_VALUE - 1
+                    : Integer.MAX_VALUE - 1;
+    /**
+     * A long sized collection cannot be larger than this value for binary
+     * search insertion point to work.
+     */
+    public static final long LONG_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT
+            = Long.MAX_VALUE + Long.MIN_VALUE > 0
+                    ? -Long.MIN_VALUE - 1
+                    : Long.MAX_VALUE - 1;
+
+    ;
+    
+    /**
+     * Binary search insertion point modes, when the element is in the collection.
+     * 
+     * <p>Example: e[0]=1, e[1]=1, e[2]=1. 
+     * If {@link BinarySearchInsertionMode#NONE} then return  1. 
+     * If {@link BinarySearchInsertionMode#FIRST} then return 0. 
+     * If {@link BinarySearchInsertionMode#LAST} then return 3.</p>
+     */
+    public static enum BinarySearchInsertionMode {
+        /**
+         * First found index is returned
+         */
+        NONE,
+        /**
+         * Iterate to the first matching object from the found position and
+         * return that index.
+         */
+        FIRST,
+        /**
+         * Iterator to the last matching object from the found position and
+         * return that index.
+         */
+        LAST
+    }
 
     /**
      * This method sorts the specified {@link IntIndexedGetterSetter} object
@@ -106,6 +153,58 @@ public class SortSupport {
     }
 
     /**
+     * This method sorts the specified {@link List} object using the
+     * {@link Comparator} object sent as argument. The method uses heap sort,
+     * thus it guarantees O(n*log n), is done in place, and it's not stable
+     * (does not retain the relative position of equal elements).
+     *
+     * <p>
+     * This method should be used with lists that provide optimized access to
+     * elements by index.</p>
+     *
+     * @param <T> the type of elements in the {@link List}
+     * @param list the list
+     * @param comp the comparator used to perform the sorting.
+     * @see RandomAccess
+     */
+    public static final <T> void heapSort(List<T> list, Comparator<T> comp) {
+        T tmp;
+        int n = list.size();
+        int i = n / 2;
+        int v, w;
+        while (true) {
+            if (i > 0) {
+                i--;
+                v = i;
+            } else {
+                n--;
+                if (n == 0) {
+                    return;
+                }
+                tmp = list.get(0);
+                list.set(0, list.get(n));
+                list.set(n, tmp);
+                v = 0;
+            }
+            w = 2 * v + 1;
+            while (w < n) {
+                if (w + 1 < n && comp.compare(list.get(w + 1), list.get(w)) > 0) {
+                    w++;
+                }
+                if (comp.compare(list.get(v), list.get(w)) >= 0) {
+                    break;
+                }
+                tmp = list.get(v);
+                list.set(v, list.get(w));
+                list.set(w, tmp);
+
+                v = w;
+                w = 2 * v + 1;
+            }
+        }
+    }
+
+    /**
      * The method searches for an object in the ascending sorted structure using
      * binary search. The result is either a positive value representing the
      * position where the object was found, or -1 if the object was not found.
@@ -127,12 +226,311 @@ public class SortSupport {
             mean = l + ((r - l) / 2);
             int c = comp.compare(search, gs.get(mean));
             if (c > 0) {
-                if(mean==Integer.MAX_VALUE) {
+                if (mean == Integer.MAX_VALUE) {
                     return -1;
                 }
                 l = mean + 1;
             } else if (c < 0) {
-                if(mean==0) {
+                if (mean == 0) {
+                    return -1;
+                }
+                r = mean - 1;
+            } else {
+                return mean;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Does a binary search to find the insertion point in an ascending order
+     * sorted collection.
+     *
+     *
+     * <p>
+     * This method will fail if the size of the collection is larger than the
+     * given {@link #INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT} limit.</p>
+     *
+     * <p>
+     * If the search mode is {@link BinarySearchInsertionMode#NONE} and the
+     * search element is found in the collection, the position of the element
+     * will be returned as the insertion point.</p>
+     *
+     * * <p>
+     * The insertion point is calculated from the function's return like this:
+     * <code>insertion point = result &lt; 0 ? -(result + 1) : result</code></p>
+     *
+     *
+     * @param <T> the type of elements in the collection
+     * @param gs the collection direct access methods
+     * @param comp the comparator instance
+     * @param search the object to search for
+     * @param mode the insertion point search mode
+     * @return the insertion point as a positive value if the object was found
+     * in the collection, and (-1 - insertion point) otherwise. If the value is
+     * negative, the insertion point is obtained by taking the absolute value of
+     * the result of adding 1 to the return value.
+     * @throws IllegalStateException if the size assertions on insertion failed.
+     */
+    public static final <T> int binarySearchInsertionPoint(
+            IntIndexedGetterSetter<T> gs, Comparator<T> comp, T search,
+            BinarySearchInsertionMode mode) throws IllegalStateException {
+
+        if (gs.size() > INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT) {
+            throw new IllegalArgumentException(String.format(
+                    "INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT: %d. Actual: %d",
+                    INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT, gs.size()));
+        }
+        int l = 0;
+        int r = gs.size() - 1;
+        int mean;
+        int ins = 0;
+        while (l <= r) {
+            mean = l + (r - l) / 2;
+            int c = comp.compare(search, gs.get(mean));
+            if (c > 0) {
+                if (mean == INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT) {
+                    return -1 - INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT;
+                }
+                l = mean + 1;
+                ins = l;
+            } else if (c < 0) {
+                if (mean == 0) {
+                    return -1;
+                }
+                r = mean - 1;
+                ins = r;
+            } else {
+                switch (mode) {
+                    case FIRST:
+                        T mObj = gs.get(mean);
+                        while (mean > 0 && Objects.
+                                equals(mObj, gs.get(mean - 1))) {
+                            mean--;
+                        }
+                        break;
+                    case LAST:
+                        mObj = gs.get(mean);
+                        int sz = gs.size() - 1;
+                        while (mean < sz && Objects.equals(mObj, gs.
+                                get(mean + 1))) {
+                            mean++;
+                        }
+                        break;
+                }
+                return mean;
+            }
+        }
+        if (ins <= 0) {
+            return -1;
+        } else {
+            return -1 - ins;
+        }
+    }
+
+    /**
+     * Does a binary search to find the insertion point in an ascending order
+     * sorted collection.
+     *
+     *
+     * <p>
+     * This method will fail if the size of the collection is larger than the
+     * given {@link #LONG_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT} limit.</p>
+     *
+     * <p>
+     * If the search mode is {@link BinarySearchInsertionMode#NONE} and the
+     * search element is found in the collection, the position of the element
+     * will be returned as the insertion point.</p>
+     *
+     * <p>
+     * The insertion point is calculated from the function's return like this:
+     * <code>insertion point = result &lt; 0 ? -(result + 1) : result</code></p>
+     *
+     * @param <T> the type of elements in the collection
+     * @param gs the collection direct access methods
+     * @param comp the comparator instance
+     * @param search the object to search for
+     * @param mode the insertion point search mode
+     * @return the insertion point as a positive value if the object was found
+     * in the collection, and (-1 - insertion point) otherwise. If the value is
+     * negative, the insertion point is obtained by taking the absolute value of
+     * the result of adding 1 to the return value.
+     * @throws IllegalStateException if the size assertions on insertion failed.
+     */
+    public static final <T> long binarySearchInsertionPoint(
+            LongIndexedGetterSetter<T> gs, Comparator<T> comp, T search,
+            BinarySearchInsertionMode mode) throws IllegalStateException {
+
+        if (gs.size() > LONG_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT) {
+            throw new IllegalArgumentException(String.format(
+                    "LONG_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT: %d. Actual: %d",
+                    LONG_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT, gs.size()));
+        }
+        long l = 0;
+        long r = gs.size() - 1;
+        long mean;
+        long ins = 0;
+        while (l <= r) {
+            mean = l + (r - l) / 2;
+            int c = comp.compare(search, gs.get(mean));
+            if (c > 0) {
+                if (mean == INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT) {
+                    return -1 - INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT;
+                }
+                l = mean + 1;
+                ins = l;
+            } else if (c < 0) {
+                if (mean == 0) {
+                    return -1;
+                }
+                r = mean - 1;
+                ins = r;
+            } else {
+                switch (mode) {
+                    case FIRST:
+                        T mObj = gs.get(mean);
+                        while (mean > 0 && Objects.
+                                equals(mObj, gs.get(mean - 1))) {
+                            mean--;
+                        }
+                        break;
+                    case LAST:
+                        mObj = gs.get(mean);
+                        long sz = gs.size() - 1;
+                        while (mean < sz && Objects.equals(mObj, gs.
+                                get(mean + 1))) {
+                            mean++;
+                        }
+                        break;
+                }
+                return mean;
+            }
+        }
+        if (ins <= 0) {
+            return -1;
+        } else {
+            return -1 - ins;
+        }
+    }
+    
+    /**
+     * Does a binary search to find the insertion point in an ascending order
+     * sorted list.
+     *
+     *
+     * <p>
+     * This method will fail if the size of the list is larger than the
+     * given {@link #LONG_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT} limit.</p>
+     *
+     * <p>
+     * If the search mode is {@link BinarySearchInsertionMode#NONE} and the
+     * search element is found in the list, the position of the element
+     * will be returned as the insertion point.</p>
+     *
+     * <p>
+     * The insertion point is calculated from the function's return like this:
+     * <code>insertion point = result &lt; 0 ? -(result + 1) : result</code></p>
+     *
+     * @param <T> the type of elements in the list
+     * @param list the list
+     * @param comp the comparator instance
+     * @param search the object to search for
+     * @param mode the insertion point search mode
+     * @return the insertion point as a positive value if the object was found
+     * in the collection, and (-1 - insertion point) otherwise. If the value is
+     * negative, the insertion point is obtained by taking the absolute value of
+     * the result of adding 1 to the return value.
+     * @throws IllegalStateException if the size assertions on insertion failed.
+     */
+    public static final <T> long binarySearchInsertionPoint(
+            List<T> list, Comparator<T> comp, T search,
+            BinarySearchInsertionMode mode) throws IllegalStateException {
+
+        if (list.size() > INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT) {
+            throw new IllegalArgumentException(String.format(
+                    "INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT: %d. Actual: %d",
+                    INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT, list.size()));
+        }
+        int l = 0;
+        int r = list.size() - 1;
+        int mean;
+        int ins = 0;
+        while (l <= r) {
+            mean = l + (r - l) / 2;
+            int c = comp.compare(search, list.get(mean));
+            if (c > 0) {
+                if (mean == INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT) {
+                    return -1 - INT_BINARY_SEARCH_INSERTION_POINT_SIZE_LIMIT;
+                }
+                l = mean + 1;
+                ins = l;
+            } else if (c < 0) {
+                if (mean == 0) {
+                    return -1;
+                }
+                r = mean - 1;
+                ins = r;
+            } else {
+                switch (mode) {
+                    case FIRST:
+                        T mObj = list.get(mean);
+                        while (mean > 0 && Objects.
+                                equals(mObj, list.get(mean - 1))) {
+                            mean--;
+                        }
+                        break;
+                    case LAST:
+                        mObj = list.get(mean);
+                        long sz = list.size() - 1;
+                        while (mean < sz && Objects.equals(mObj, list.
+                                get(mean + 1))) {
+                            mean++;
+                        }
+                        break;
+                }
+                return mean;
+            }
+        }
+        if (ins <= 0) {
+            return -1;
+        } else {
+            return -1 - ins;
+        }
+    }
+
+    /**
+     * The method searches for an object in the ascending sorted list using
+     * binary search. The result is either a positive value representing the
+     * position where the object was found, or -1 if the object was not found.
+     *
+     * <p>
+     * This method should be used with lists that provide optimized access to
+     * elements by index.</p>
+     *
+     * @param <T> the type of elements in the list
+     * @param list the list
+     * @param comp the comparator matching the sorting order of the objects in
+     * the list
+     * @param search the object to search for
+     * @return either a positive value representing the position where the
+     * searched element was found, or -1 if the object was not found.
+     * @see RandomAccess
+     */
+    public static final <T> int binarySearch(List<T> list,
+                                             Comparator<T> comp, T search) {
+        int l = 0;
+        int r = list.size() - 1;
+        int mean;
+        while (l <= r) {
+            mean = l + ((r - l) / 2);
+            int c = comp.compare(search, list.get(mean));
+            if (c > 0) {
+                if (mean == Integer.MAX_VALUE) {
+                    return -1;
+                }
+                l = mean + 1;
+            } else if (c < 0) {
+                if (mean == 0) {
                     return -1;
                 }
                 r = mean - 1;
@@ -163,14 +561,14 @@ public class SortSupport {
         long mean;
         while (l <= r) {
             mean = l + ((r - l) / 2);
-            long c = comp.compare(search, gs.get(mean));
+            int c = comp.compare(search, gs.get(mean));
             if (c > 0) {
-                if(mean==Long.MAX_VALUE) {
+                if (mean == Long.MAX_VALUE) {
                     return -1;
                 }
                 l = mean + 1;
             } else if (c < 0) {
-                if(mean==0) {
+                if (mean == 0) {
                     return -1;
                 }
                 r = mean - 1;
@@ -180,7 +578,5 @@ public class SortSupport {
         }
         return -1;
     }
-    
-    
 
 }
---
+++ src/test/java/org/ticdev/toolboxj/collections/SortSupportTest.java
@@ -0,0 +1,293 @@
+package org.ticdev.toolboxj.collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Supplier;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * Test class for {@link SortSupport}
+ *
+ * @author <a href="mailto:tandauioan@gmail.com">Ioan - Ciprian Tandau</a>
+ */
+public class SortSupportTest {
+
+    /**
+     * String comparator for sorting - ascending order
+     */
+    private final static Comparator<String> VALUES_COMPARATOR = (s1, s2) -> s1.
+            compareTo(s2);
+
+    /**
+     * Unique strings supplier
+     */
+    private final static Supplier<String> VALUES_SUPPLIER = new Supplier<String>() {
+
+        long currentValue = Long.MIN_VALUE;
+
+        @Override
+        public String get() {
+            String result = currentValue + "";
+            currentValue++;
+            return result;
+        }
+    };
+
+    /**
+     * Generates an array of the given length filled with the same value
+     *
+     * @param length the length of the array
+     * @return the array
+     */
+    private static String[] newArraySameValues(int length) {
+        String[] result = new String[length];
+        String value = VALUES_SUPPLIER.get();
+        for (int i = 0; i < length; i++) {
+            result[i] = value;
+        }
+        return result;
+    }
+
+    /**
+     * Returns a new array of the given length that contains unique strings
+     *
+     * @param length the length of the array
+     * @return the new array
+     */
+    private static String[] newArrayUniqueValues(int length) {
+        String[] result = new String[length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = VALUES_SUPPLIER.get();
+        }
+        return result;
+    }
+
+    /**
+     * Returns a new array of the given length filled with different strings and
+     * a pseudo-random effort to introduce duplicates.
+     *
+     * @param length the length of the new array
+     * @return the new array
+     */
+    private static String[] newArrayWithDuplicates(int length) {
+        String[] result = new String[length];
+        ArrayList<String> reuseSet = new ArrayList<>(length);
+        Random random = new Random(System.currentTimeMillis());
+        for (int i = 0; i < result.length; i++) {
+            String value;
+            if (reuseSet.size() > 0) {
+                int choice = random.nextInt(reuseSet.size());
+                value = (choice % 2 == 0) ? VALUES_SUPPLIER.get() : reuseSet.
+                        remove(choice);
+            } else {
+                value = VALUES_SUPPLIER.get();
+            }
+            result[i] = value;
+            reuseSet.add(value);
+        }
+        return result;
+    }
+
+    /**
+     * Uses a pseudo random number generator to shuffle the content of the
+     * array, in place.
+     *
+     * @param array the array to shuffle
+     * @return the array, shuffled
+     */
+    private static String[] shuffle(String[] array) {
+        Random random = new Random(System.currentTimeMillis());
+        int sz = array.length;
+        while (sz-- >= 0) {
+            int to = random.nextInt(array.length);
+            int from = random.nextInt(array.length);
+            if (to != from) {
+                String tmp = array[to];
+                array[to] = array[from];
+                array[from] = tmp;
+            }
+        }
+        return array;
+    }
+
+    private final static List<String[]> ORIGINAL_SAME_VALUE = new LinkedList<>();
+
+    private final static List<String[]> ORIGINAL_DUPLICATES = new LinkedList<>();
+
+    private final static List<String[]> ORIGINAL_UNIQUES = new LinkedList<>();
+
+    private final static int MAX_ITERATIONS_CAPACITY = 50;
+
+    /**
+     * Initialize original test arrays
+     */
+    @Before
+    public void before() {
+
+        for (int capacity = 1; capacity <= MAX_ITERATIONS_CAPACITY; capacity++) {
+            ORIGINAL_SAME_VALUE.add(newArraySameValues(capacity));
+            ORIGINAL_DUPLICATES.add(shuffle(newArrayWithDuplicates(capacity)));
+            ORIGINAL_UNIQUES.add(shuffle(newArrayUniqueValues(capacity)));
+        }
+    }
+
+    /**
+     * Test
+     * {@link SortSupport#heapSort(org.ticdev.toolboxj.collections.IntIndexedGetterSetter, java.util.Comparator)}
+     */
+    @Test
+    public void test_heapSort_intIndexed() {
+
+        /*
+         * array has same value
+         */
+        for (String[] original : ORIGINAL_SAME_VALUE) {
+            final String[] expected = Arrays.copyOf(original, original.length);
+            final String[] actual = Arrays.copyOf(original, original.length);
+            Arrays.sort(expected);
+            SortSupport.heapSort(ArraySupport.indexedGetterSetterOf(actual),
+                    VALUES_COMPARATOR);
+            Assert.assertArrayEquals(expected, actual);
+        }
+
+        /*
+         * array has some duplicates
+         */
+        for (String[] original : ORIGINAL_DUPLICATES) {
+            final String[] expected = Arrays.copyOf(original, original.length);
+            final String[] actual = Arrays.copyOf(original, original.length);
+            Arrays.sort(expected);
+            SortSupport.heapSort(ArraySupport.indexedGetterSetterOf(actual),
+                    VALUES_COMPARATOR);
+            Assert.assertArrayEquals(expected, actual);
+        }
+
+        /*
+         * array has unique values
+         */
+        for (String[] original : ORIGINAL_UNIQUES) {
+            final String[] expected = Arrays.copyOf(original, original.length);
+            final String[] actual = Arrays.copyOf(original, original.length);
+            Arrays.sort(expected);
+            SortSupport.heapSort(ArraySupport.indexedGetterSetterOf(actual),
+                    VALUES_COMPARATOR);
+            Assert.assertArrayEquals(expected, actual);
+
+            // TODO remove the commented code below
+//            System.out.println("---- START:" + original.length);
+//            for (int i = 0; i < original.length; i++) {
+//                System.out.println(
+//                        i + "\t" + original[i] + "\t" + expected[i] + "\t" + actual[i]);
+//            }
+//            System.out.println("---- DONE:" + original.length);
+//            System.out.println();
+        }
+    }
+
+    /**
+     * Test
+     * {@link SortSupport#heapSort(org.ticdev.toolboxj.collections.LongIndexedGetterSetter, java.util.Comparator)}
+     */
+    @Test
+    public void test_heapSort_longIndexed() {
+
+        /*
+         * array has same value
+         */
+        for (String[] original : ORIGINAL_SAME_VALUE) {
+            final String[] expected = Arrays.copyOf(original, original.length);
+            final String[] actual = Arrays.copyOf(original, original.length);
+            Arrays.sort(expected);
+            SortSupport.heapSort(LongIndexedGetterSetter.wrap(ArraySupport.
+                    indexedGetterSetterOf(actual)),
+                    VALUES_COMPARATOR);
+            Assert.assertArrayEquals(expected, actual);
+        }
+
+        /*
+         * array has some duplicates
+         */
+        for (String[] original : ORIGINAL_DUPLICATES) {
+            final String[] expected = Arrays.copyOf(original, original.length);
+            final String[] actual = Arrays.copyOf(original, original.length);
+            Arrays.sort(expected);
+            SortSupport.heapSort(LongIndexedGetterSetter.wrap(ArraySupport.
+                    indexedGetterSetterOf(actual)),
+                    VALUES_COMPARATOR);
+            Assert.assertArrayEquals(expected, actual);
+        }
+
+        /*
+         * array has unique values
+         */
+        for (String[] original : ORIGINAL_UNIQUES) {
+            final String[] expected = Arrays.copyOf(original, original.length);
+            final String[] actual = Arrays.copyOf(original, original.length);
+            Arrays.sort(expected);
+            SortSupport.heapSort(LongIndexedGetterSetter.wrap(ArraySupport.
+                    indexedGetterSetterOf(actual)),
+                    VALUES_COMPARATOR);
+            Assert.assertArrayEquals(expected, actual);
+        }
+    }
+
+    /**
+     * Test {@link SortSupport##heapSort(java.util.List, java.util.Comparator)}
+     */
+    @Test
+    public void test_heapSort_list() {
+
+        /*
+         * array has same value
+         */
+        ORIGINAL_SAME_VALUE.stream().
+                forEach((original) -> {
+                    final String[] expected = Arrays.copyOf(original,
+                            original.length);
+                    String[] actual = Arrays.copyOf(original, original.length);
+                    Arrays.sort(expected);
+                    List<String> list = Arrays.asList(actual);
+                    SortSupport.heapSort(list, VALUES_COMPARATOR);
+                    actual = list.toArray(actual);
+                    Assert.assertArrayEquals(expected, actual);
+                });
+
+        /*
+         * array has some duplicates
+         */
+        ORIGINAL_DUPLICATES.stream().
+                forEach((original) -> {
+                    final String[] expected = Arrays.copyOf(original,
+                            original.length);
+                    String[] actual = Arrays.copyOf(original, original.length);
+                    Arrays.sort(expected);
+                    List<String> list = Arrays.asList(actual);
+                    SortSupport.heapSort(list, VALUES_COMPARATOR);
+                    actual = list.toArray(actual);
+                    Assert.assertArrayEquals(expected, actual);
+                });
+
+        /*
+         * array has unique values
+         */
+        ORIGINAL_UNIQUES.stream().
+                forEach((original) -> {
+                    final String[] expected = Arrays.copyOf(original,
+                            original.length);
+                    String[] actual = Arrays.copyOf(original, original.length);
+                    Arrays.sort(expected);
+                    List<String> list = Arrays.asList(actual);
+                    SortSupport.heapSort(list, VALUES_COMPARATOR);
+                    actual = list.toArray(actual);
+                    Assert.assertArrayEquals(expected, actual);
+                });
+    }
+
+}
