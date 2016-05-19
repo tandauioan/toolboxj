cad99ba635736db079bc3f10c19122f212eb9844
John Tandau <tandau@liquidoperations.com>
2016-05-19 12:59:53 -0400

    changed methods and method signatures

--- src/main/java/org/ticdev/toolboxj/collections/Graph.java
+++ src/main/java/org/ticdev/toolboxj/collections/Graph.java
@@ -1,5 +1,7 @@
 package org.ticdev.toolboxj.collections;
 
+import java.util.Set;
+
 /**
  *
  * Graph interface.
@@ -11,48 +13,102 @@ package org.ticdev.toolboxj.collections;
 public interface Graph<VERTEX, EDGE> {
 
     /**
-     * Returns an {@link Iterable} instance that allows iteration over all the
-     * nodes that can be reached from this node via an edge.
+     * Returns true if the vertex exists in the graph and false otherwise.
      *
-     * @param node the node
-     * @return {@link Iterable} over the neighbors of this node
-     * @throws IllegalArgumentException if the node is not recognized as a graph
-     * vertex
+     * @param vertex the vertex
+     * @return true if the vertex exists in the graph and false otherwise.
      */
-    public Iterable<VERTEX> getNeighbors(VERTEX node) throws
-            IllegalArgumentException;
+    boolean hasVertex(VERTEX vertex);
 
     /**
-     * Returns an {@link Iterable} instance that allows iteration over all the
-     * vertices from node1 to node2.
+     * Like {@link #hasVertex(java.lang.Object)} but throws an
+     * {@link IllegalArgumentException} if the vertex doesn't exist in this
+     * graph.
      *
-     * @param node1 the first node
-     * @param node2 the second node
-     * @return {@link Iterable} over the edges
-     * @throws IllegalArgumentException if the nodes are not recognized as graph
-     * vertices
+     * @param vertex the vertex
+     * @throws IllegalArgumentException if the vertex does not exist in this
+     * graph
      */
-    public Iterable<EDGE> getEdges(VERTEX node1, VERTEX node2) throws
-            IllegalArgumentException;
+    void assertHasVertex(VERTEX vertex) throws IllegalArgumentException;
 
     /**
-     * Returns a single edge from node1 to node2. The choice of edge in a set of
-     * edges is implementation defined.
+     * Returns a set of all the vertices in this graph.
      *
-     * @param node1 the first node
-     * @param node2 the second node
-     * @return an edge, or null if there are no edges from node1 to node2
-     * @throws IllegalArgumentException if the nodes are not recognized as graph
-     * vertices
+     * @return a set of all the vertices in this graph.
      */
-    public EDGE getOneEdge(VERTEX node1, VERTEX node2) throws
-            IllegalArgumentException;
+    Set<VERTEX> vertices();
 
     /**
-     * Returns all the nodes/vertices in this graph.
+     * Returns the number of vertices in this graph
      *
-     * @return all the nodes in this graph
+     * @return the number of vertices in this graph
      */
-    public Iterable<VERTEX> getVertices();
+    int vertexCount();
+
+    /**
+     * Returns all the vertices that are connected to this graph via at least
+     * one edge, regardless of direction.
+     *
+     * @param vertex the starting vertex
+     * @param excludeSelf if true, the current node is excluded from the set
+     * even if there is an edge to itself
+     * @return all the vertices connected to this vertex
+     */
+    Set<VERTEX> connected(VERTEX vertex, boolean excludeSelf);
+
+    /**
+     * Returns all the vertices that can reach this vertex via at least one
+     * edge. The direction is taken into account, if applicable.
+     *
+     * @param vertex the vertex that is reached
+     * @param excludeSelf true if the vertex itself should be excluded even if
+     * there is an edge pointing to itself.
+     * @return all the vertices that can reach this vertex via at least one edge
+     */
+    Set<VERTEX> reaching(VERTEX vertex, boolean excludeSelf);
+
+    /**
+     * Returns all the vertices that can be reached from this vertex via at
+     * least one edge. The direction is take into account, if applicable.
+     *
+     * @param vertex the vertex that is reached
+     * @param excludeSelf true if the vertex itself should be excluded even if
+     * there is an edge pointing to itself.
+     * @return all the vertices that can be reached from this vertex via at
+     * least one edge.
+     */
+    Set<VERTEX> reachableFrom(VERTEX vertex, boolean excludeSelf);
+
+    /**
+     * Returns true if an edge exists from vertex1 to vertex2.
+     *
+     * @param vertex1 the starting vertex
+     * @param vertex2 the ending vertex
+     * @return true if an edge exists from vertex1 to vertex2 and false
+     * otherwise.
+     */
+    boolean edgeExists(VERTEX vertex1, VERTEX vertex2);
+
+    /**
+     * Returns all the edges starting in vertex1 and ending in vertex2
+     *
+     * @param vertex1 the starting vertex
+     * @param vertex2 the ending vertex
+     * @return all the edges starting in vertex1 and ending in vertex2
+     */
+    Set<EDGE> edges(VERTEX vertex1, VERTEX vertex2);
+
+    /**
+     * Returns one edge starting in vertex1 and ending in vertex2.
+     *
+     * If there are multiple edges between the two nodes, the implementation can
+     * choose what to return.
+     *
+     * @param vertex1 the starting vertex
+     * @param vertex2 the ending vertex
+     * @return one edge starting from vertex1 and ending in vertex2. If there is
+     * no edge, null is returned.
+     */
+    EDGE oneEdge(VERTEX vertex1, VERTEX vertex2);
 
 }
