25bf1e65701eac9d1b4fdd97a53f5498e1332a32
John Tandau <tandau@liquidoperations.com>
2016-07-18 13:12:56 -0400

    added more methods to BooleanWrapper

--- src/main/java/org/ticdev/toolboxj/primitives/BooleanWrapper.java
+++ src/main/java/org/ticdev/toolboxj/primitives/BooleanWrapper.java
@@ -2,7 +2,7 @@ package org.ticdev.toolboxj.primitives;
 
 /**
  * Mutable boolean primitive holder.
- * 
+ * <p/>
  * <p>
  * Non boolean specific integer methods (byte, short, int, long) are accepted
  * with the rule that a value of zero resolved to false, and a value different
@@ -10,19 +10,18 @@ package org.ticdev.toolboxj.primitives;
  * for false and 1 for true. All the other methods will throw an unsupported
  * operation exception.
  * </p>
- * 
+ * <p/>
  * <p>
  * {@link #hashCode()} and {@link #equals(Object)} adjust to the current value
  * of the holder.
  * </p>
- * 
- * @author <a href="mailto:tandauioan@gmail.com">Ioan - Ciprian Tandau</a>
  *
+ * @author <a href="mailto:tandauioan@gmail.com">Ioan - Ciprian Tandau</a>
  */
 public class BooleanWrapper
-    implements
-    PrimitiveSetter<BooleanWrapper>,
-    PrimitiveGetter<BooleanWrapper> {
+        implements
+        PrimitiveSetter<BooleanWrapper>,
+        PrimitiveGetter<BooleanWrapper> {
 
     /**
      * serial version
@@ -36,12 +35,11 @@ public class BooleanWrapper
 
     /**
      * Class constructor. Initializes the value to the initial value.
-     * 
-     * @param initialValue
-     *            the initial value of the primitive.
+     *
+     * @param initialValue the initial value of the primitive.
      */
     public BooleanWrapper(
-        boolean initialValue) {
+            boolean initialValue) {
         this.value = initialValue;
     }
 
@@ -89,19 +87,22 @@ public class BooleanWrapper
     @Override
     public float floatValue() {
         throw new UnsupportedOperationException(
-            PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING.apply(this));
+                PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING
+                        .apply(this));
     }
 
     @Override
     public double doubleValue() {
         throw new UnsupportedOperationException(
-            PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING.apply(this));
+                PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING
+                        .apply(this));
     }
 
     @Override
     public char charValue() {
         throw new UnsupportedOperationException(
-            PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING.apply(this));
+                PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING
+                        .apply(this));
     }
 
     @Override
@@ -134,26 +135,107 @@ public class BooleanWrapper
     @Override
     public BooleanWrapper floatValue(float value) {
         throw new UnsupportedOperationException(
-            PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING.apply(this));
+                PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING
+                        .apply(this));
     }
 
     @Override
     public BooleanWrapper doubleValue(double value) {
         throw new UnsupportedOperationException(
-            PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING.apply(this));
+                PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING
+                        .apply(this));
     }
 
     @Override
     public BooleanWrapper charValue(char value) {
         throw new UnsupportedOperationException(
-            PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING.apply(this));
+                PrimitiveGetter.FORMAT_UNSUPPORTED_EXCEPTION_STRING
+                        .apply(this));
+    }
+
+    /**
+     * Sets the value to true.
+     *
+     * @return this instance
+     */
+    public BooleanWrapper setTrue() {
+        value = true;
+        return this;
+    }
+
+    /**
+     * Returns true if the value is true and false otherwise
+     *
+     * @return true if the value is true and false otherwise
+     */
+    public boolean isTrue() {
+        return value;
+    }
+
+    /**
+     * Returns true if the value is false and false otherwise
+     *
+     * @return true if the value is false and false otherwise
+     */
+    public boolean isFalse() {
+        return !value;
+    }
+
+    /**
+     * Gets the current value then sets it to the given value
+     *
+     * @param newValue the new value
+     * @return the current value before setting to the new value
+     */
+    public boolean getThenSet(boolean newValue) {
+        boolean result = value;
+        value = newValue;
+        return result;
+    }
+
+    /**
+     * Inverts the value, i.e. if the value is true it becomes false, and
+     * vice-versa.
+     *
+     * @return this instance
+     */
+    public BooleanWrapper invert() {
+        value = !value;
+        return this;
+    }
+
+    /**
+     * Returns the current value then inverts it.
+     * @return the current value before inverting it.
+     */
+    public boolean getThenInvert() {
+        boolean result=value;
+        value=!value;
+        return result;
+    }
+
+    /**
+     * Inverts the current value and returns it
+     * @return the inverted value
+     */
+    public boolean invertThenGet() {
+        return invert().isTrue();
+    }
+
+    /**
+     * Sets the value to false
+     *
+     * @return this instance
+     */
+    public BooleanWrapper setFalse() {
+        value = false;
+        return this;
     }
 
     /**
      * Creates a new instance of BooleanHolder from the given value.
-     * 
-     * @param value
-     *            the value
+     *
+     * @param value the value
      * @return the new instance
      */
     public static BooleanWrapper of(boolean value) {
@@ -162,13 +244,12 @@ public class BooleanWrapper
 
     /**
      * Creates a new instance of BooleanHolder from the given PrimitiveGetter.
-     * 
-     * @param primitiveGetter
-     *            the primitive getter
+     *
+     * @param primitiveGetter the primitive getter
      * @return the new instance
      */
-    public static  BooleanWrapper
-        of(PrimitiveGetter<?> primitiveGetter) {
+    public static BooleanWrapper
+    of(PrimitiveGetter<?> primitiveGetter) {
         return of(primitiveGetter.booleanValue());
     }
 
@@ -187,7 +268,7 @@ public class BooleanWrapper
     public boolean equals(Object obj) {
         try {
             return (obj instanceof PrimitiveGetter<?>
-                && value == ((PrimitiveGetter<?>) obj).booleanValue());
+                    && value == ((PrimitiveGetter<?>) obj).booleanValue());
         } catch (Exception ex) {
             return false;
         }
@@ -197,5 +278,6 @@ public class BooleanWrapper
     public String toString() {
         return value + "";
     }
+
 
 }
